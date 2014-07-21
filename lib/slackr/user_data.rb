3d1b4214236b1edea8739f6fb0e0cf3af0ccf4d6
anna.lesniak@lunarlogic.io <anna.lesniak@lunarlogic.io>
2014-07-21 20:23:07 +0100

    -

--- lib/slackr/user_data.rb
+++ lib/slackr/user_data.rb
@@ -37,7 +37,7 @@ module Slackr
         raise Slackr::ServiceError, "Slack.com - #{response.code} - #{response.body}"
       else
         users = JSON.parse(response.body)["members"]
-        users.select {|user| id.to_s == user["id"]
+        user = users.select {|user| id.to_s == user["id"]}
           case size
             when 32
               avatar_url = user["profile"]["image_32"]
